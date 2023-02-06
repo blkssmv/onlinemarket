37ce7e8e120b8abeae965376c05b7bc62f3c0ac4
bekzat <bekzat@avantage.one>
2023-02-06 17:41:17 +0600

    back

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,12 @@
-const express = require('express')
+const Router = require('express')
 
-const router = express.Router()
+const router = new Router()
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check)
+router.get('/auth', authMiddleware, userController.check)
+
 
 module.exports = router
